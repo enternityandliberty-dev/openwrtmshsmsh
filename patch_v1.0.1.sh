f6ecd2b0105e287a77d8d7757ef64171a91764b1
enternityandliberty-dev <enternityandliberty@gmail.com>
2026-04-25 18:57:20 +0300

    Update patch_v1.0.1.sh

--- patch_v1.0.1.sh
+++ patch_v1.0.1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Patch v1.0.1.test"
-uci set network.wg0.listen_port='51888'
+uci set network.wg0.listen_port='51999'
 uci commit network
 echo "Patch is applied, wait for router to restart"
 /etc/init.d/network restart
