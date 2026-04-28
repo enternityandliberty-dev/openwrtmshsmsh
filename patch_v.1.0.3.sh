2b255527fc8b5414c485818519e182ceddb737f6
enternityandliberty-dev <enternityandliberty@gmail.com>
2026-04-28 15:39:37 +0300

    Create patch_v.1.0.3.sh
    Added VK IP addresses for direct routing

---
+++ patch_v.1.0.3.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "Patch v1.0.2.test"
+
+#vk ip
+uci add_list homeproxy.control.wan_direct_ipv4_ips='95.142.204.188/32'
+uci add_list homeproxy.control.wan_direct_ipv4_ips='95.213.56.1/32'
+uci add_list homeproxy.control.wan_direct_ipv4_ips='93.186.237.1/32'
+uci add_list homeproxy.control.wan_direct_ipv4_ips='87.240.139.193/32'
+uci add_list homeproxy.control.wan_direct_ipv4_ips='87.240.137.207/32'
+uci add_list homeproxy.control.wan_direct_ipv4_ips='87.240.190.70/32'
+uci add_list homeproxy.control.wan_direct_ipv4_ips='87.240.137.206/32'
+uci add_list homeproxy.control.wan_direct_ipv4_ips='87.240.129.140/32'
+uci add_list homeproxy.control.wan_direct_ipv4_ips='93.186.225.205/32'
+uci add_list homeproxy.control.wan_direct_ipv4_ips='87.240.190.75/32'
+uci add_list homeproxy.control.wan_direct_ipv4_ips='87.240.137.130/32'
+uci add_list homeproxy.control.wan_direct_ipv4_ips='87.240.137.208/32'
+
+uci commit homeproxy
+echo "Patch is applied, wait for the services to restart"
+/etc/init.d/homeproxy restart
