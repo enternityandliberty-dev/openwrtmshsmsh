f335295d13f6b03d6593a104a4fae057996bbb2f
enternityandliberty-dev <enternityandliberty@gmail.com>
2026-04-28 16:15:22 +0300

    Update patch_v.development.sh

--- patch_v.development.sh
+++ patch_v.development.sh
@@ -4,9 +4,11 @@
 PATCH_VERSION="1.0.4test"
 VERSION_FILE="/root/version"
 
+# UI
 echo "Patch v.${PATCH_VERSION}"
 echo "This patch adds VK IP addresses to homeproxy direct list"
 
+# Проверка существования файла и запись в него последней версии
 if [ ! -f "$VERSION_FILE" ]; then
     echo "${PATCH_VERSION}" > "$VERSION_FILE"
     echo "Created version file with version ${PATCH_VERSION}"
@@ -15,7 +17,11 @@ else
     echo "Added version ${PATCH_VERSION} to existing version file"
 fi
 
+#Тело скрипта
+
 echo "Patch is applied, wait for the services to restart"
+
+#Перезагрузка необходимых служб
 
 echo "Finished"
 
